ef04d6e014a85e6bb7bcd54ee81fcc38aa006feb
Martin Tolkiehn <mtolkiehn@gmail.com>
2017-05-30 16:38:53 +0200

    add test rest api call to resources

--- src/hackathon-res-jira-connector.js
+++ src/hackathon-res-jira-connector.js
@@ -1,6 +1,7 @@
 // With ES6
 import JiraApi from 'jira-client';
 import babel_polyfill from "babel-polyfill";
+import request from 'supertest'
 
 const hackathonResJiraConnector = {
   greet: async function() {
@@ -13,6 +14,14 @@ const hackathonResJiraConnector = {
       strictSSL: true
     });
     return await jira.searchJira('assignee=mtolkiehn') // jira.searchJira('issue=RX-9853') jira.searchJira('Created>2017-05-30')
+  },
+  put: async function() {
+    const app = `https://resources-staging.rplan.com`
+    const payload = '{“name”:“AMT AG”,“avatarLogic”:null,“accountType”:“t”,“active”:true,“legacyId”:319,“deleted”:false,“snapshot”:false,“owner”:“6687”,“labels”:[{“id”:“58f65f0a574cbc54621c9645",“text”:“Elektriker”,“abbrev”:null,“deleted”:false,“snapshot”:false,“color”:“1”,“account”:“58f65f0a574cbc54621c9627”,“__type”:“label”},{“id”:“58f65f0a574cbc54621c9646”,“text”:“Engineering”,“abbrev”:null,“deleted”:false,“snapshot”:false,“color”:“4",“account”:“58f65f0a574cbc54621c9627",“__type”:“label”},{“id”:“58f65f0a574cbc54621c9647",“text”:“Mechaniker”,“abbrev”:null,“deleted”:false,“snapshot”:false,“color”:“3”,“account”:“58f65f0a574cbc54621c9627”,“__type”:“label”},{“text”:“Me”,“abbrev”:null,“deleted”:false,“snapshot”:false,“color”:“6",“account”:“58f65f0a574cbc54621c9627",“__type”:“label”,“id”:“tmp$3"},{“text”:“Me1",“abbrev”:null,“deleted”:false,“snapshot”:false,“color”:“7”,“account”:“58f65f0a574cbc54621c9627”,“__type”:“label”,“id”:“tmp$4”}],“memberType”:“58f65f0a574cbc54621c9637”,“members”:[“58f65f0a574cbc54621c9648”,“58f65f0a574cbc54621c964c”,“58f65f0a574cbc54621c9650”,“58f65f0a574cbc54621c9654",“58f65f0a574cbc54621c9658”,“58f65f0a574cbc54621c965c”,“58f65f0a574cbc54621c9660”,“58f65f0a574cbc54621c9664",“58f65f0a574cbc54621c9668”,“58f65f0a574cbc54621c966c”,“58f65f0a574cbc54621c9670”,“58f65f0a574cbc54621c9674",“58f65f0a574cbc54621c9678”,“58f65f0a574cbc54621c967c”,“58f65f0a574cbc54621c9680”,“58f65f0a574cbc54621c9684",“58f65f0a574cbc54621c9688”,“58f65f0a574cbc54621c968c”,“58f65f0a574cbc54621c9690”,“58f65f0a574cbc54621c9694"],“entityTypes”:[“58f65f0a574cbc54621c9637",“58f65f0a574cbc54621c9640”,“586fbfb9e4b08b38b6c20f12"],“absencePlanningObjectType”:“58f65f0a574cbc54621c9628",“defaultWorkingScheme”:“58f65f0a574cbc54621c9636",“workingSchemes”:[“58f65f0a574cbc54621c9636"],“defaultHolidayScheme”:“58f65f0a574cbc54621c9635",“holidaySchemes”:[“58f65f0a574cbc54621c9635"],“__type”:“account”,“id”:“58f65f0a574cbc54621c9627"}'
+    return await request(app)
+      .put(`/v2/model/boards/5728c30ee4b02f1ed5cbf211/changes/0`)
+      .set('Authorization', 'bearer d23c2185-a49b-4a91-ba23-3caa74abd8e3')
+      .send(payload)
   }
 };
 
--- test/unit/hackathon-res-jira-connector.js
+++ test/unit/hackathon-res-jira-connector.js
@@ -15,13 +15,19 @@ describe('hackathonResJiraConnector', () => {
   describe('Greet function', () => {
     beforeEach(() => {
       spy(hackathonResJiraConnector, 'greet');
+      spy(hackathonResJiraConnector, 'put');
     });
 
-    it('should have been run once', async function() {
+    it.skip('should have been run once', async function() {
       const result = await hackathonResJiraConnector.greet();
       console.log(result)
       console.log('Search-Result: ' + JSON.stringify(result, null, 4));
     });
+
+    it('should post data to resource', async function() {
+      const result = await hackathonResJiraConnector.put();
+      console.log(result)
+    })
 
   });
 });
