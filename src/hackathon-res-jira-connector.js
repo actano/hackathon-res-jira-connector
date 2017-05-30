1dd6b7b8a0f46be6c793449ae956fed712abbcd6
Lars Menzel <Lars.Menzel@menzelapps.com>
2017-05-30 15:01:56 +0200

    Get all issues of RES

--- src/hackathon-res-jira-connector.js
+++ src/hackathon-res-jira-connector.js
@@ -12,7 +12,7 @@ const hackathonResJiraConnector = {
       apiVersion: '2',
       strictSSL: true
     });
-    const result = await jira.searchJira('assignee=lmenzel AND Created>2017-05-12')  // searchJira('assignee=mtolkiehn') jira.searchJira('issue=RX-9853')
+    const result = await jira.searchJira('project=RES')  // searchJira('assignee=mtolkiehn') jira.searchJira('issue=RX-9853') jira.searchJira('Created>2017-05-30')
       .then(function(result) {
         console.log(result)
         console.log('Search-Result: ' + JSON.stringify(result, null, 4));
